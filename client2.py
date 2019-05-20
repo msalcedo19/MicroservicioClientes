c63218a5aad30695d14ce33f3333aab2f4d77be9
Mateo Salcedo <m.salcedo@uniandes.edu.co>
2019-05-20 15:01:21 -0500

    first commit

---
+++ client2.py
@@ -0,0 +1,107 @@
+import json
+import uuid
+
+import pika
+
+
+# Clase para manejar obtener el JWT
+class MessageBrokerClient:
+
+    def __init__(self, queue_name):
+        # El key del canal
+        self.routing_key = queue_name
+        # Las credenciales del servidor RabbitMQ
+        self.credentials = pika.PlainCredentials('atpos', 'atpos')
+
+        # La información de conexión con la dirección del servidor
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='157.230.14.37', port=5672, virtual_host='/', credentials=self.credentials))
+
+        # Inicia el canal
+        self.channel = self.connection.channel()
+
+        # Inicia una cola para recibir la respuesta
+        result = self.channel.queue_declare('', exclusive=True)
+        self.callback_queue = result.method.queue
+
+        # Empieza a escuchar sobre el canal de respuesta
+        self.channel.basic_consume(
+            queue=self.callback_queue,
+            on_message_callback=self.on_response,
+            auto_ack=True)
+
+    # Si recibe una respuesta sobre el canal de respuestas con su id guarda la respuesta
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    # Manda un mensaje de autenticación y espera la respuesta
+    def send_message(self, body):
+        # Iniciliza la respuesta como None y un id para identificar su respuesta en el canal
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+
+        # Publica el nombre de usuario y contraseña en el canal para que el servidor lo autentique
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.routing_key,
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id,
+            ),
+            body=body)
+
+        # Espera a recibir la respuesta y la devuelve
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+# while True:
+#     # Obtiene un token
+#     obtain_token = MessageBrokerClient(queue_name='obtener_jwt')
+#     body = json.dumps({'user': '1234', 'password': str(5)})
+#     response = obtain_token.send_message(body)
+#     print(" [.] Got %r" % response)
+#
+#     token = json.loads(response)['token']
+#
+#     # Valida el token
+#     validate_token = MessageBrokerClient(queue_name='validar_jwt')
+#     body = json.dumps({'token': token})
+#     response = validate_token.send_message(body)
+#     print(" [.] Got %r" % response)
+#
+#     time.sleep(0.01)
+#     break
+
+
+def login(username, password):
+    obtain_token_broker = MessageBrokerClient(queue_name='obtener_jwt')
+    body = json.dumps({'user': username, 'password': password})
+    response = obtain_token_broker.send_message(body)
+    response = json.loads(response)
+
+    if 'error' in response:
+        raise Exception('Login fallido')
+
+    else:
+        return response
+
+
+def validate_token(token):
+    validate_token_broker = MessageBrokerClient(queue_name='validar_jwt')
+    body = json.dumps({'token': token})
+    response = validate_token_broker.send_message(body)
+    response = json.loads(response)
+
+    if 'error' in response:
+        raise Exception('Login fallido')
+
+    else:
+        return response
+
+
+#token = login('1234', '5')['token']
+#validate = validate_token(token)
+#print(validate)
---
+++ setup.py
@@ -0,0 +1,108 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask import request
+import json
+from bson import ObjectId
+from bson.json_util import dumps
+import client2
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+app.config["MONGO_URI"] = "mongodb://atpos_user:atpos_password@cluster0-shard-00-00-j6ym9.mongodb.net:27017,cluster0-shard-00-01-j6ym9.mongodb.net:27017,cluster0-shard-00-02-j6ym9.mongodb.net:27017/clientes?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+mongo = PyMongo(app)
+users = mongo.db.users
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+@app.route('/cliente/', methods=['POST', 'DELETE', 'GET'])
+def cliente():
+    print(request.headers)  
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get("usuario", "")
+        usuario = users.find_one({"usuario": username})
+        if usuario is None:
+            id_user = users.insert(data)
+            return JSONEncoder().encode(users.find_one({"_id": id_user}))
+        else:
+            return "Ya existe un cliente con el usuario: %s" % username
+    elif request.method == 'DELETE':
+        data = request.get_json()
+        username = data.get("usuario", "")
+        usuario = users.find_one({"usuario": username})
+        if usuario is None:
+            return "No existe un cliente con el usuario %s" % username
+        else:
+            users.delete_one({"usuario": username})
+            return "El cliente con el usuario %s fue Eliminado exitosamente" % username
+    elif request.method == 'GET':
+        data = request.get_json()
+        puntos = data.get("puntos", 0)
+        if type(puntos) is not int:
+            data.pop("puntos")
+            for key in puntos.keys():
+                if key == "menor":
+                    query = {"puntos": {"$lte": puntos.get(key)}}
+                elif key == "mayor":
+                    query = {"puntos": {"$gte": puntos.get(key)}}
+                else:
+                    vals = puntos.get(key)
+                    query = {"puntos": {"$lte": vals[1]}}
+                    data.update(query)
+                    menores = users.find(data)
+                    resp = []
+                    for men in menores:
+                        if men["puntos"] >= vals[0]:
+                            resp.append(men)
+                    return JSONEncoder().encode(resp)
+                data.update(query)
+        usuarios = users.find(data)
+        return dumps(usuarios)
+
+
+@app.route('/cliente/agregarPuntos', methods=['PUT'])
+def agregarPuntos():
+    if request.method == 'PUT':
+        data = request.get_json()
+        print(request.headers)
+        username = data.get("usuario", "")
+        puntos = data.get("puntos", 0)
+
+        user = users.find_one({"usuario": username})
+        if user is None:
+            return "El cliente con usuario %s no existe" % username
+        else:
+            puntos_user = user["puntos"]
+            total = puntos_user + abs(puntos)
+            users.update_one({"usuario": username}, {"$set": {"puntos": total}})
+            return "Se agrego exitosamente %s puntos al cliente con usuario %s" % (abs(puntos), username)
+
+
+@app.route('/cliente/canjearPuntos', methods=['PUT'])
+def canjearPuntos():
+    if request.method == 'PUT':
+        data = request.get_json()
+        print(request.headers)
+        username = data.get("usuario", "")
+        puntos = data.get("puntos", 0)
+
+        user = users.find_one({"usuario": username})
+        if user is None:
+            return "El cliente con usuario %s no existe" % username
+        else:
+            puntos_user = user["puntos"]
+            if puntos_user >= abs(puntos):
+                total = puntos_user - abs(puntos)
+                users.update_one({"usuario": username}, {"$set": {"puntos": total}})
+                return "Fueron Canjeados %s puntos al cliente con usuario %s" % (abs(puntos), username)
+            else:
+                return "El cliente con usuario %s no tiene la cantidad de puntos suficientes" % username
+
+
